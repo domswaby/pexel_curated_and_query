c016b9803449299ff5d7d300ccda1b3f59552d6e
ds <swaby@protonmail.com>
2023-04-12 14:46:44 -0400

    build curated and search routes

--- backend/controllers/pexelsController.js
+++ backend/controllers/pexelsController.js
@@ -1,9 +1,22 @@
+import { createClient } from "pexels";
 
-export const searchImages = () => {
+const client = createClient(
+  "tW63Ibum32iszaRYyxq5BNt8WlXsSuqfT0dfk03sXXacygbFRNRyexuz"
+);
 
-}
+export const searchImages = async (req, res) => {
+    let page = req.query.page || 1;
+    let per_page = req.query.per_page;
+    let query = req.query.query
 
-export const curatedImages = () => {
+    const photos = await client.photos.search({ query, page, per_page })
+    res.json(photos);
+};
 
-}
+export const curatedImages = async (req, res) => {
+  let page = req.query.page;
+  let per_page = req.query.per_page;
 
+  const photos = await client.photos.curated({ page, per_page });
+  res.json(photos);
+};
