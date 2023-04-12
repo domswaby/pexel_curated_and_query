c5522fcdeaa7217b094f74dcae2bf08f286eb439
ds <swaby@protonmail.com>
2023-04-12 16:48:21 -0400

    clear out some boiler plate and add header and search components

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">Welcome to Pexels Curated and Search!</div>
+  )
+}
+
+export default Header
