49e91d9b77350d86734a9057545df73414e979d5
ds <swaby@protonmail.com>
2023-04-13 14:42:06 -0400

    make navigation responsive

--- frontend/src/components/Paginator/Paginator.js
+++ frontend/src/components/Paginator/Paginator.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
-import "./Paginator.css"
+import "./Paginator.css";
+import { useMediaQuery, useTheme } from "@mui/material";
 
-const Paginator = ({ search, curatedPage, searchedPage, setCuratedPage, setSearchedPage, myRef, totalCuratedResults, totalSearchedResults }) => {
+const Paginator = ({
+  search,
+  curatedPage,
+  searchedPage,
+  setCuratedPage,
+  setSearchedPage,
+  myRef,
+  totalCuratedResults,
+  totalSearchedResults,
+}) => {
+  const theme = useTheme();
+  const small = useMediaQuery(theme.breakpoints.down("sm"));
 
   const changePage = (e, pageNumber) => {
     if (search) {
@@ -17,11 +29,15 @@ const Paginator = ({ search, curatedPage, searchedPage, setCuratedPage, setSearc
   return (
     <Stack spacing={2} className="paginator-stack">
       <Pagination
-        count={search ? Math.ceil(totalSearchedResults / 10) : Math.ceil(totalCuratedResults / 10)}
+        count={
+          search
+            ? Math.ceil(totalSearchedResults / 10)
+            : Math.ceil(totalCuratedResults / 10)
+        }
         page={search ? searchedPage : curatedPage}
         color="secondary"
         onChange={changePage}
-        size="large"
+        size={small ? "small" : "large"}
         variant="outlined"
         shape="rounded"
       />
